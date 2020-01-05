464f1beb9e3ddd3d71463457e0e4fc81bd896797
José Chacón <josejch11@gmail.com>
2020-01-05 18:07:02 -0500

    first commit

---
+++ restTestApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ResttestappConfig(AppConfig):
+    name = 'restTestApp'
---
+++ restTestApp/urls.py
@@ -0,0 +1,32 @@
+"""restTest URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+# Serializers define the API representation.
+from rest_framework.urlpatterns import format_suffix_patterns
+from restTestApp import views
+
+
+urlpatterns = [
+    path('', views.api_root),
+    path('snippets/', views.SnippetList.as_view(), name='snippets-list'),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view()),
+    path('user/', views.UserList.as_view(), name='users-list'),
+    path('user/<int:pk>/', views.UserDetail.as_view()),
+
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
